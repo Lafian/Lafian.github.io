ef30a922f6d7bf064b55a08d0023c3958bb233ff
Lafian <46103510+Lafian@users.noreply.github.com>
2019-06-09 01:07:07 +0600

    lol

--- localhost/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<form action='upload.php' method='POST'
-encytype="multipart/from-data"><!-- encytpe="multipart/from-data" нужен для загрузки файлов!!!-->
-file: <input type='file' name="filename">
-<input type='submit' value='send'>
-</form>
-</body>
-</html>
