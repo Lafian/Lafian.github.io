e5ec562dcb9dd99673443a38ba23199dff3681b0
Lafian <46103510+Lafian@users.noreply.github.com>
2020-02-25 01:26:27 +0600

    ggg

--- chessjsplay/chess2.js
+++
@@ -1,8 +0,0 @@
-for (var i=0;i<8;++i) {
- document.write("<img src=\"l.png\">");
- for (var j=0;j<8;++j) document.write("<span onclick=\"hu("+j+","+i+")\" id=\""+j+i+"\"><img></span>");
- document.write("<img src=\"r.png\"><br>");
-}
-
-
-
